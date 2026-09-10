931a11d154552ca37526418c528482aaa6a10d3e
AKSHAT THAKUR <165548880+AKSHATTHAKUR2145@users.noreply.github.com>
2026-09-11 00:16:29 +0530

    AI enviornment setup

---
+++ AIEnvironmentSetup.java
@@ -0,0 +1,20 @@
+
+public class AIEnvironmentSetup {
+
+    public static void main(String[] args) {
+
+        System.out.println("AI Lab Environment Setup");
+        System.out.println("------------------------");
+
+        System.out.println("Java Version: "
+                + System.getProperty("java.version"));
+
+        System.out.println("Operating System: "
+                + System.getProperty("os.name"));
+
+        System.out.println("Java Architecture: "
+                + System.getProperty("os.arch"));
+
+        System.out.println("\nEnvironment setup successful!");
+    }
+}
